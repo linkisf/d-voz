a810324912e64485d5b8eda8cefe0215c56df534
Lincoln <lincoln.sf11@gmail.com>
2023-11-11 21:07:50 -0300

    Subindo primeiros arquivos

---
+++ D-Voz1/D-Voz1/Controllers/PaginaInicial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace D_Voz1.Controllers
+{
+    public class PaginaInicial : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Denuncie()
+        {
+            return View();
+        }
+    }
+}
