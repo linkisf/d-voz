77d7da2d8c3f1c7d336f1caf1323b04e9f80795b
Lincoln <lincoln.sf11@gmail.com>
2023-11-17 16:55:29 -0300

    teste de verificacao do botao no menunav e teste de navegacao para pagina de denuncia

---
+++ D-Voz1/src/Dvoz.Testes/DenunciaNavegacao.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dvoz.Testes
+{
+    public class DenunciaNavegacao
+    {
+        [Fact]
+        public void CarregaPaginaInicialVerificaExistenciaLinkDenuncie()
+        {
+            //Arrange
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            //Act
+            driver.Navigate().GoToUrl("http://localhost:7226/");
+            //Assert
+            Assert.Contains("Denuncie", driver.PageSource);
+        }
+        [Fact]
+        public void AcessandoPaginaDenuncieMenuNav()
+        {
+            IWebDriver driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            driver.Navigate().GoToUrl("http://localhost:7226/");
+            driver.Manage().Window.Size = new System.Drawing.Size(1918, 1030);
+            driver.FindElement(By.LinkText("Denuncie")).Click();
+        }
+    }
+}
